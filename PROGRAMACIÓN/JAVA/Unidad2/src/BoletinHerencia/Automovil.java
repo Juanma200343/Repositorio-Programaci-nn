66e8c6c2dee3d26fd926f91471efdb6267fb9d38
Juanma200343 <juanma200343@gmail.com>
2025-02-10 20:37:21 +0100

    Ejercicio Herencias

--- PROGRAMACIÓN/JAVA/Unidad2/src/BoletinHerencia/Automovil.java
+++ PROGRAMACIÓN/JAVA/Unidad2/src/BoletinHerencia/Automovil.java
@@ -1,62 +1,40 @@
-package BoletinHerencia;
+package BoletinHerencias;
 
-public class Automovil extends Vehiculo {
-
-	/**
-	 * @param dueno
-	 * @param numPuertas
-	 * @param numRuedas
-	 */
+public class Automovil extends Vehiculos {
 	public Automovil(String dueno, int numPuertas, int numRuedas) {
-		super();
+		super(dueno, numRuedas, numRuedas);
 		this.dueno = dueno;
 		this.numPuertas = numPuertas;
 		this.numRuedas = numRuedas;
 	}
-	
 	String dueno;
-	int numPuertas;
+	int numPuertas ; 
 	int numRuedas;
-	
-	
-	@Override
-	public String toString() {
-		return "Automovil [dueno=" + dueno + ", numPuertas=" + numPuertas + ", numRuedas=" + numRuedas + "]";
-	}
-
 
 	public String getDueno() {
 		return dueno;
 	}
-
-
 	public void setDueno(String dueno) {
 		this.dueno = dueno;
 	}
-
-
 	public int getNumPuertas() {
 		return numPuertas;
 	}
-
-
 	public void setNumPuertas(int numPuertas) {
 		this.numPuertas = numPuertas;
 	}
-
-
 	public int getNumRuedas() {
 		return numRuedas;
 	}
-
-
 	public void setNumRuedas(int numRuedas) {
 		this.numRuedas = numRuedas;
 	}
 	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Automovil [dueno=" + dueno + ", numPuertas=" + numPuertas + ", numRuedas=" + numRuedas + ", getDueno()="
+				+ getDueno() + ", getNumPuertas()=" + getNumPuertas() + ", getNumRuedas()=" + getNumRuedas() + "]";
+	}
 	
 	
 }
--- PROGRAMACIÓN/JAVA/Unidad2/src/BoletinHerencia/Camion.java
+++ PROGRAMACIÓN/JAVA/Unidad2/src/BoletinHerencia/Camion.java
@@ -1,19 +1,8 @@
-package BoletinHerencia;
+package BoletinHerencias;
 
-public class Camion extends Vehiculo {
-
-	/**
-	 * @param dueno
-	 * @param numPuertas
-	 * @param numRuedas
-	 * @param tonelajeKilos
-	 * @param numPasajeros
-	 * @param numEjes
-	 */
-	
+public class Camion extends Vehiculos {
 	public Camion(String dueno, int numPuertas, int numRuedas, int tonelajeKilos, int numPasajeros, int numEjes) {
-		
-		super();
+		super(dueno, numEjes, numRuedas);
 		this.dueno = dueno;
 		this.numPuertas = numPuertas;
 		this.numRuedas = numRuedas;
@@ -21,24 +10,21 @@ public class Camion extends Vehiculo {
 		this.numPasajeros = numPasajeros;
 		this.numEjes = numEjes;
 	}
+
+
+
 	String dueno;
-	int numPuertas;
+	int numPuertas ; 
 	int numRuedas;
 	int tonelajeKilos;
 	int numPasajeros;
 	int numEjes;
 	
 	
-	@Override
-	public String toString() {
-		return "Camion [dueno=" + dueno + ", numPuertas=" + numPuertas + ", numRuedas=" + numRuedas + ", tonelajeKilos="
-				+ tonelajeKilos + ", numPasajeros=" + numPasajeros + ", numEjes=" + numEjes + "]";
-	}
-
-
 	public String getDueno() {
 		return dueno;
 	}
+
 
 
 	public void setDueno(String dueno) {
@@ -46,9 +32,11 @@ public class Camion extends Vehiculo {
 	}
 
 
+
 	public int getNumPuertas() {
 		return numPuertas;
 	}
+
 
 
 	public void setNumPuertas(int numPuertas) {
@@ -56,9 +44,11 @@ public class Camion extends Vehiculo {
 	}
 
 
+
 	public int getNumRuedas() {
 		return numRuedas;
 	}
+
 
 
 	public void setNumRuedas(int numRuedas) {
@@ -66,9 +56,11 @@ public class Camion extends Vehiculo {
 	}
 
 
+
 	public int getTonelajeKilos() {
 		return tonelajeKilos;
 	}
+
 
 
 	public void setTonelajeKilos(int tonelajeKilos) {
@@ -76,9 +68,11 @@ public class Camion extends Vehiculo {
 	}
 
 
+
 	public int getNumPasajeros() {
 		return numPasajeros;
 	}
+
 
 
 	public void setNumPasajeros(int numPasajeros) {
@@ -86,13 +80,26 @@ public class Camion extends Vehiculo {
 	}
 
 
+
 	public int getNumEjes() {
 		return numEjes;
 	}
 
 
+
 	public void setNumEjes(int numEjes) {
 		this.numEjes = numEjes;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Camion [dueno=" + dueno + ", numPuertas=" + numPuertas + ", numRuedas=" + numRuedas + ", tonelajeKilos="
+				+ tonelajeKilos + ", numPasajeros=" + numPasajeros + ", numEjes=" + numEjes + ", getDueno()="
+				+ getDueno() + ", getNumPuertas()=" + getNumPuertas() + ", getNumRuedas()=" + getNumRuedas()
+				+ ", getTonelajeKilos()=" + getTonelajeKilos() + ", getNumPasajeros()=" + getNumPasajeros()
+				+ ", getNumEjes()=" + getNumEjes() + "]";
 	}
 	
 	
